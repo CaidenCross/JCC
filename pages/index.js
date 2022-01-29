698072402f404ba1a09423008c05c9faf7e25f94
Spence1225 <tulivisremod@gmail.com>
2022-01-28 23:17:37 -0500

    changes

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,9 @@ export default function Home() {
         <>
           <div>
             <h1>HELLO CARI</h1>
+            <HTMLElement classname={styles.styles.scss}>
+                
+            </HTMLElement>
            <p> A p line such as this one will be used as a paragraph,
                so you may type whatever you want in this.
            </p>
