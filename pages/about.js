f4e17bdc6e33ffb6bb6712deefe1d2110aa48aa1
Lacira <cariguttenberg@gmail.com>
2022-01-29 00:07:33 -0500

    coding

---
+++ pages/about.js
@@ -0,0 +1,26 @@
+import e from "express";
+
+
+let input = document.querySelector('input');
+let container = document.querySelector('.container');
+let colors = ['brown', 'red', 'black'];
+
+input.addEventListener('keyup', (e) ==>)
+
+
+
+
+function About() {
+return (
+
+
+
+
+
+)
+
+
+
+}
+
+export default About
