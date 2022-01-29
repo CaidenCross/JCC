43e52b8724d7f04b6c447d0424d60320fbe1142a
Spence1225 <tulivisremod@gmail.com>
2022-01-28 22:25:27 -0500

    fixed my code lines for cari

--- pages/index.js
+++ pages/index.js
@@ -8,23 +8,7 @@ export default function Home() {
            <p> A p line such as this one will be used as a paragraph,
                so you may type whatever you want in this.
            </p>
-
-
-
-          </div>
+         </div>
         </>
-
-
-
-
-
-
-
-
-
-
-
-
-
-    )
+ )
 }
